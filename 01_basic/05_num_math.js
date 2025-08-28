45512c82fdc84dbe4decd40c5f0f672ab91b1879
Aniket2790 <tiwatrianiket605@gmail.com>
2025-08-29 00:53:57 +0530

    maths_min_max

--- 01_basic/05_num_math.js
+++ 01_basic/05_num_math.js
@@ -16,9 +16,32 @@ console.log(a ** 2);
 
 ///////////////////////math////////////////
 console.log(Math.PI);
+//give pi value
 console.log(Math.E);
+//give e value
 console.log(Math.sqrt(16));
+//give square root value
 console.log(Math.abs(-5));
+
+// give ceiling value
 console.log(Math.ceil(5.1));
+//give floor value
 console.log(Math.floor(5.9));
-console.log(Math.random());
+//give random value
+
+console.log("this is random value");
+console.log(Math.floor(Math.random() * 10 + 2));
+//give maximum value
+console.log(Math.max(5, 10));
+
+//give minimum value
+console.log(Math.min(5, 10));
+
+//give boolean value
+console.log(Math.random() < 0.5);
+//give  boolean value
+console.log(Math.random() < 0.5);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
