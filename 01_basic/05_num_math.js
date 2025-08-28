d43f0df279b11bc4b2f2ce82bfebbb66d3b5f60b
Aniket2790 <tiwatrianiket605@gmail.com>
2025-08-29 00:43:51 +0530

    df

---
+++ 01_basic/05_num_math.js
@@ -0,0 +1,24 @@
+console.log(5 + 10);
+console.log(5 - 10);
+console.log(5 * 10);
+console.log(5 / 10);
+console.log(5 % 10);
+console.log(5 ** 2);
+
+let a = 5;
+let b = 10;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** 2);
+
+///////////////////////math////////////////
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.sqrt(16));
+console.log(Math.abs(-5));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(5.9));
+console.log(Math.random());
